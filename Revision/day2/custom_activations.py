28ea52d0cde2e32a6d1c5d1fabddd60cdf3c7c87
amacharla15 <amacharla@csuchico.edu>
2026-04-04 13:00:53 -0700

    custom leaky relu pytorch

--- Revision/day2/custom_activations.py
+++ Revision/day2/custom_activations.py
@@ -1,16 +1,25 @@
 class CustomReLU(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input):
-        # save what you need for backward
-        # ctx.save_for_backward(input)
-        # return the result
         ctx.save_for_backward(input)
         return torch.clamp(input,min=0)
 
     @staticmethod
     def backward(ctx, grad_output):
-
         originalinput,=ctx.saved_tensors
         relugradient=torch.where(originalinput<0,originalinput*0,1)
         return grad_output*relugradient
 
+class CustomLeakyReLU(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, alpha):
+        ctx.save_for_backward(input)
+        ctx.alpha=alpha
+        return torch.where(input<0,input*alpha,input)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        originalinput,=ctx.saved_tensors
+        alpha=ctx.alpha
+        relugradient=torch.where(originalinput<0,alpha,1)
+        return relugradient*grad_output, None
