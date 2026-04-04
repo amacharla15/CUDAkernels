8a18498c58613cc75c6b26760e1fc930633ac8a0
amacharla15 <amacharla@csuchico.edu>
2026-04-04 02:06:38 -0700

    relu

--- Revision/day2/Relu.py
+++ Revision/day2/Relu.py
@@ -1,7 +1,3 @@
-#Relu Function:
-#if any value in the tensor is lesser than 0 we make it 0 and >0 we leave them alone
-# Relu function mainly used in neural networks for to make non linear relationships between weights/features
-
 
 
 import torch
