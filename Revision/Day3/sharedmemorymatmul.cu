d6a4df0b4835265fc7c8d6bd050c456abe965554
amacharla15 <amacharla@csuchico.edu>
2026-04-08 09:03:16 -0700

    4 X 4 matrix works completely fine and a screenshot of outputs in the NVIDIA A100 runs folder

--- Revision/Day3/sharedmemorymatmul.cu
+++ Revision/Day3/sharedmemorymatmul.cu
@@ -3,12 +3,13 @@
 #include <cmath>
 using namespace std;
 
-__global__ void naive_matmul(const float* A, const float* B, float* C, int M, int N, int K) {
+__global__ void naive_matmul(const float* A, const float* B, float* C, int M, int N, int K) { //naive matmul kernel 
     int col = blockIdx.x * blockDim.x + threadIdx.x;
     int row = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (row < M && col < K) {
-        float sum1 = 0.0f;
+        float sum1 = 0.0f; //K is dimension of the matrix and small k is running index could be replaced with temp but 
+        // I usually use k as running index
         for (int k = 0; k < N; k++) {
             sum1 = sum1 + A[row * N + k] * B[k * K + col];
         }
@@ -16,6 +17,64 @@ __global__ void naive_matmul(const float* A, const float* B, float* C, int M, in
     }
 }
 
+__global__ void shared_matmul(const float*A, const float*B, float*C, int M, int N, int K){
+    int row= blockIdx.y * blockDim.y + threadIdx.y;
+    int col= blockIdx.x * blockDim.x + threadIdx.x;
+
+    int local_row= threadIdx.y;
+    int local_col= threadIdx.x; // blocklevel for shared memory
+    
+    const int tile_size = 16;
+
+    __shared__ float tile_A[16][16];
+    __shared__ float tile_B[16][16];
+
+    float sum1 = 0.0f;
+
+    int num_phases=(N+tile_size-1)/tile_size;
+
+    for (int phases=0;phases<num_phases;phases++){ // phases for each block
+        int a_global_row = row;
+        int a_global_col = phases * tile_size + local_col;
+                                                        // each tile loads values according to this formula/way
+        int b_global_row = phases * tile_size + local_row;
+        int b_global_col = col;
+
+        //shared memory loading  for A and B 
+        // 2D is stored as 1D in memory so " a_global_row * N + a_global_col" 
+        if (a_global_row < M && a_global_col < N) {
+            tile_A[local_row][local_col] = A[a_global_row * N + a_global_col];
+        } else {
+            tile_A[local_row][local_col] = 0.0f;
+        }
+        // 2D is stored as 1D in memory so " b_global_row * K + b_global_col" 
+        if (b_global_row < N && b_global_col < K) {
+            tile_B[local_row][local_col] = B[b_global_row * K + b_global_col];
+        } else {
+            tile_B[local_row][local_col] = 0.0f;
+        }
+        // all threads need to sync 
+        __syncthreads();
+
+        // actual dot product 
+        for (int t = 0; t < tile_size; t++) {
+            sum1 = sum1 + tile_A[local_row][t] * tile_B[t][local_col];
+        }
+
+        //this is because we create one shared-memory array for the block not different tiles , such as 4 tiles with size of 2
+        // if matrices are of 4X4 size , so reuse of arrays will occur and other threads may rewrite one arrays values when
+        //computation is going on 
+        __syncthreads();
+
+        
+    }
+    if (row < M && col < K) {
+        C[row * K + col] = sum1;
+    }
+
+
+}
+ // cpu version of matmul just to check if the kernels output matches our local matmul works for any matrix
 void reference_matmul(float A[][4], float B[][4], float C[][4], int M, int N, int K) {
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < K; j++) {
@@ -27,7 +86,7 @@ void reference_matmul(float A[][4], float B[][4], float C[][4], int M, int N, in
         }
     }
 }
-
+// debug function used to print our matrices
 void print_matrix_4x4(float X[][4], int rows, int cols) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
@@ -36,7 +95,7 @@ void print_matrix_4x4(float X[][4], int rows, int cols) {
         cout << endl;
     }
 }
-
+// comparison matrix for simpler way of printing if the matrices matches or not 
 bool compare_matrix_4x4(float X[][4], float Y[][4], int rows, int cols) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
@@ -49,14 +108,17 @@ bool compare_matrix_4x4(float X[][4], float Y[][4], int rows, int cols) {
 }
 
 int main() {
+    // sizes of our matrics , can be replaced with any valid dimension numbers 
     int M = 4;
     int N = 4;
     int K = 4;
 
+    //matrices
     float A[4][4];
     float B[4][4];
     float C_ref[4][4];
-    float C_gpu[4][4];
+    float C_naive[4][4];
+    float C_shared[4][4];
 
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
@@ -72,26 +134,39 @@ int main() {
 
     reference_matmul(A, B, C_ref, M, N, K);
 
+    //pointers to our gpu memory 
     float* d_A;
     float* d_B;
-    float* d_C;
+    float* d_C; 
 
+
+    //memory allocation
     cudaMalloc((void**)&d_A, M * N * sizeof(float));
     cudaMalloc((void**)&d_B, N * K * sizeof(float));
     cudaMalloc((void**)&d_C, M * K * sizeof(float));
 
+    //copying the memory to gpu
+
     cudaMemcpy(d_A, A, M * N * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, B, N * K * sizeof(float), cudaMemcpyHostToDevice);
 
+    //block dimensions and threads per block 
     dim3 threadsPerBlock(16, 16);
     dim3 blocksPerGrid((K + threadsPerBlock.x - 1) / threadsPerBlock.x,
                        (M + threadsPerBlock.y - 1) / threadsPerBlock.y);
-
+// kernel launch 
     naive_matmul<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, M, N, K);
-    cudaDeviceSynchronize();
+    cudaDeviceSynchronize(); // wait until kernels finish
 
-    cudaMemcpy(C_gpu, d_C, M * K * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(C_naive, d_C, M * K * sizeof(float), cudaMemcpyDeviceToHost); // copy back our results
 
+    shared_matmul<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, M, N, K);
+    cudaDeviceSynchronize(); // wait until kernels finish
+
+    cudaMemcpy(C_shared, d_C, M * K * sizeof(float), cudaMemcpyDeviceToHost); // copy back our results
+
+
+    //debug statements
     cout << "Matrix A:" << endl;
     print_matrix_4x4(A, M, N);
 
@@ -105,14 +180,34 @@ int main() {
 
     cout << endl;
     cout << "GPU Naive C:" << endl;
-    print_matrix_4x4(C_gpu, M, K);
+    print_matrix_4x4(C_naive, M, K);
 
     cout << endl;
-    if (compare_matrix_4x4(C_ref, C_gpu, M, K)) {
-        cout << "CPU and GPU results match" << endl;
+    cout << "GPU Shared C:" << endl;
+    print_matrix_4x4(C_shared, M, K);
+
+    cout << endl;
+    if (compare_matrix_4x4(C_ref, C_naive, M, K)) {
+        cout << "CPU and GPU Naive results match" << endl;
     } else {
-        cout << "CPU and GPU results do not match" << endl;
+        cout << "CPU and GPU Naive results do not match" << endl;
     }
+
+    cout << endl;
+    if (compare_matrix_4x4(C_ref, C_shared, M, K)) {
+        cout << "CPU and GPU Shared results match" << endl;
+    } else {
+        cout << "CPU and GPU Shared results do not match" << endl;
+    }
+
+    cout << endl;
+    if (compare_matrix_4x4(C_naive, C_shared, M, K)) {
+        cout << "GPU Naive and GPU Shared results match" << endl;
+    } else {
+        cout << "GPU Naive and GPU Shared results do not match" << endl;
+    }
+
+    //freeing the memory
 
     cudaFree(d_A);
     cudaFree(d_B);
